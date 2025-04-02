d616bdc8fd6e822b741ea4901e7e9a139c64d4d4
Unknown Developer <138309645+Shivam230708@users.noreply.github.com>
2025-04-02 11:18:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,18 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
+def get_int_env(var_name, default=0):
+    try:
+        return int(environ.get(var_name, default))
+    except ValueError:
+        return default
+
+API_ID = get_int_env("API_ID")
 API_HASH = environ.get("API_HASH", "")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-OWNER_ID = int(environ.get("OWNER_ID", ""))
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
+OWNER_ID = get_int_env("OWNER_ID")
+LOG_CHANNEL = get_int_env("LOG_CHANNEL")
 MONGO_DB_URI = environ.get("MONGO_DB_URI", "")
-PORT = int(environ.get('PORT', 8080))
+PORT = get_int_env("PORT", 8080)
+
 AUTH_CHANNELS = environ.get("AUTH_CHANNEL", "")
-AUTH_CHANNELS = [int(channel_id) for channel_id in AUTH_CHANNELS.split(",")]
+AUTH_CHANNELS = [int(channel_id) for channel_id in AUTH_CHANNELS.split(",") if channel_id.isdigit()]
